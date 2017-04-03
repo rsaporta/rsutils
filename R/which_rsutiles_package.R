a770f4052f88dea60c56bdb6da50c5b84bdd2a1c
Rick Saporta <rick.saporta@vydia.com>
2017-04-03 11:58:44 -0400

    which_rsutils_package

--- R/which_rsutiles_package.R
+++ R/which_rsutiles_package.R
@@ -1,4 +1,4 @@
 which_rsutiles_package <- function() {
-  message("This came from rsutils3")
-  return("rsutils3")
+  message("This came from rsutils --- the newest one!   (If you are seeing this, all is good)")
+  return("rsutils -- the newest one 2017-04-02")
 }
