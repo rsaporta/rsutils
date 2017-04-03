3ae47b1ee884cb5f7da665f14afd226077a3baf8
Rick Saporta <rsaporta@gmail.com>
2017-04-03 10:12:58 -0400

    rs_install_all_packages

---
+++ R/rs_install_all_packages.R
@@ -0,0 +1,23 @@
+.rs_install_all_packages <- function() {
+  devtools::install_github("rsaporta/rsuaspath",        dependencies=FALSE)
+  devtools::install_github("rsaporta/rsuaws",           dependencies=FALSE)
+  devtools::install_github("rsaporta/rsubitly",         dependencies=FALSE)
+  devtools::install_github("rsaporta/rsuconsoleutils",  dependencies=FALSE)
+  devtools::install_github("rsaporta/rsucurl",          dependencies=FALSE)
+  devtools::install_github("rsaporta/rsudb",            dependencies=FALSE)
+  devtools::install_github("rsaporta/rsudict",          dependencies=FALSE)
+  devtools::install_github("rsaporta/rsujesus",         dependencies=FALSE)
+  devtools::install_github("rsaporta/rsunotify",        dependencies=FALSE)
+  devtools::install_github("rsaporta/rsuorchard",       dependencies=FALSE)
+  devtools::install_github("rsaporta/rsuplotting",      dependencies=FALSE)
+  devtools::install_github("rsaporta/rsuprophesize",    dependencies=FALSE)
+  devtools::install_github("rsaporta/rsuscrubbers",     dependencies=FALSE)
+  devtools::install_github("rsaporta/rsushiny",         dependencies=FALSE)
+  devtools::install_github("rsaporta/rsutils2",         dependencies=FALSE)
+  devtools::install_github("rsaporta/rsuvydia",         dependencies=FALSE)
+  devtools::install_github("rsaporta/rsuworkspace",     dependencies=FALSE)
+  devtools::install_github("rsaporta/rsuxls",           dependencies=FALSE)
+  devtools::install_github("rsaporta/rsutils3",         dependencies=FALSE)
+}
+
+
