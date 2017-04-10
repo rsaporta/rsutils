dfd7d346a274146ffd664f3197d7e3242e6782f7
Rick Saporta <rick.saporta@vydia.com>
2017-04-10 17:18:12 -0400

    messaging

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,9 +1,10 @@
 .onLoad <- function(libname, pkgname) {
-  cat("Running .onLoad() from rsutils")
+  try(require(colorout))
+  cat("Running .onLoad() from rsutils --------------------------------- ======================================= ||\n")
 
   load_rsutils()
 }
 
 testing_utils <- function() {
-  cat("yes, this file loaded.")
+  cat("yes, this file loaded. -- it came from '<path>/rsutils/R/onLoad.R'\n")
 }
