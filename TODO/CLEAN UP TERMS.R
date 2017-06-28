4bc75ee0d56a17b37997d8d137abe24b4f7dcb94
Rick Saporta <rick.saporta@vydia.com>
2017-06-28 14:19:48 -0400

    Clean Up Terms TODO

---
+++ TODO/CLEAN UP TERMS.R
@@ -0,0 +1,35 @@
+CLEAN UP TERMS
+
+----------------------------------------------------
+  "allow_null"
+  "null_ok"
+  "na_allowed"
+  "blank_allowed"
+  EG: 
+  isValidColumnName(DT, col=byCols,       minLength=1, allow_null=byCols_can_be_NULL)
+  is.character_of_length1(N.nm, null_ok=FALSE, blank_allowed=FALSE)
+
+----------------------------------------------------
+
+  "fail.if.not"
+  "failIfNot"
+
+----------------------------------------------------
+
+  formatbytes
+  formnumb
+  fwp
+
+
+----------------------------------------------------
+
+
+----------------------------------------------------
+
+
+
+----------------------------------------------------
+
+
+
+----------------------------------------------------
