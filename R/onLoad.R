d9c75031e3eed8512c55120af87531a3541ad5cb
Rick Saporta <rick.saporta@vydia.com>
2017-08-28 16:20:32 -0400

    .onLoad

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,5 +1,8 @@
 .onLoad <- function(libname, pkgname, verbose=getOption("verbose.rsutils_load", default=FALSE)) {
-  try(require(colorout))
+  try(require(colorout,   quietly=TRUE))
+  try(require(data.table, quietly=TRUE))
+  try(require(dplyr,      quietly=TRUE))
+  try(require(magrittr,   quietly=TRUE))
 
   if (verbose)
     cat("Running .onLoad() from rsutils --------------------------------- ======================================= ||\n")
