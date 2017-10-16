1121349cf0e34811e7d3f38b88ff20df6dde997e
Rick Saporta <rick.saporta@vydia.com>
2017-10-16 11:26:12 -0400

    onLoad cat ~> packageStartupMessage

--- R/AAA__onLoad.R
+++ R/AAA__onLoad.R
@@ -1,11 +1,15 @@
-.onLoad <- function(libname, pkgname, verbose=getOption("verbose.rsutils_load", default=FALSE)) {
+.onLoad <- function(libname, pkgname) {
+  verbose <- getOption("verbose.rsutils_load"
+                      , default = getOption("verbose.rsutils_load"
+                         , default = TRUE))
+  
   try(require(colorout,   quietly=TRUE))
   try(require(data.table, quietly=TRUE))
   try(require(dplyr,      quietly=TRUE))
   try(require(magrittr,   quietly=TRUE))
 
   if (verbose)
-    cat(sprintf("Running .onLoad() from %-16s --------------------------- ================================ ||\n", pkgname))
+    packageStartupMessage(sprintf("Running .onLoad() from %-16s --------------------------- ================================ ||\n", pkgname))
 
   load_rsutils(rsutils_load=FALSE)
 }
