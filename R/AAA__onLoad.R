eb8aa45d9212e3f1cd1f747092d3c87bf43c0884
Rick Saporta on Vydia_MBPbar18 <rick.saporta@vydia.com>
2019-02-28 11:49:40 -0500

    .onLoad() created option rsutils.installing

--- R/AAA__onLoad.R
+++ R/AAA__onLoad.R
@@ -2,15 +2,20 @@
   verbose <- getOption("verbose.rsutils_load"
                       , default = getOption("verbose.rsutils_load"
                          , default = TRUE))
-  
-  try(require(colorout,   quietly=TRUE))
-  try(require(data.table, quietly=TRUE))
-  try(require(dplyr,      quietly=TRUE))
-  try(require(magrittr,   quietly=TRUE))
 
   if (verbose)
     packageStartupMessage(sprintf("Running .onLoad() from %-16s --------------------------- ================================ ||\n", pkgname))
 
-  if (getOption("rsutils.load_on_startup", default=TRUE))
-    load_rsutils(rsutils_load=FALSE)
+  installing <- getOption("rsutils.installing", default = FALSE)
+  
+  if (!installing) {
+    try(require(colorout,   quietly=TRUE))
+    try(require(data.table, quietly=TRUE))
+    try(require(dplyr,      quietly=TRUE))
+    try(require(magrittr,   quietly=TRUE))
+
+    if (getOption("rsutils.load_on_startup", default=TRUE))
+      load_rsutils(rsutils_load=FALSE)
+
+  }
 }
