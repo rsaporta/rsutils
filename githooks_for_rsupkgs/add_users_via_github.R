bda47cb890ff00ae567b6b0223c003b434164a61
Rick Saporta on TFD_MBP_2019 <Rick@TheFarmersDog.com>
2019-12-04 08:46:33 -0500

    add_users_via_github.R

--- githooks_for_rsupkgs/add_users_via_github.R
+++ githooks_for_rsupkgs/add_users_via_github.R
@@ -1,43 +1,67 @@
-## PAT -- To create, go to:
+#!/bin/bash
+
+
+## GITUB PAT (PERSONAL ACCESS TOKEN) 
+## To create a PAT, go to:
 ##   https://github.com/settings/tokens
-##  needs just 'repo' rights
-# export GITHUB_PAT=XXXXXXXXXXX_____fill_me_in____XXXXXXXXXX
+## You only need to grant 'repo' rights
+export GITHUB_PAT=XXXXXXXXXXX_____fill_me_in____XXXXXXXXXX
 
 # ## WHOM TO ADD
-# export USER_TO_ADD=XXXXXXXXXXX_____THEIR_USER_NAME____XXXXXXXXXX
+export USER_TO_ADD=XXXXXXXXXXX_____THEIR_USER_NAME____XXXXXXXXXX
 
 # ## MY Github USER NAME
-# export REPO_OWNER=rsaporta
+export REPO_OWNER=rsaporta
+
+
+## CREATE A FILE TO EXPORT OUTPUT
+export GITHUB_API_RESPONSE_FILE="/tmp/github_api_invitation_accept_for_${USER_TO_ADD}_$(date -u +'%Y%m%dT%H%M%SZ').sh"
+
+cat << EOF > $GITHUB_API_RESPONSE_FILE
+#!/bin/bash
+
+
+## GITUB PAT (PERSONAL ACCESS TOKEN) 
+## To create a PAT, go to:
+##   https://github.com/settings/tokens
+## You only need to grant 'repo' rights
+export GITHUB_PAT=XXXXXXXXXXX_____fill_me_in____XXXXXXXXXX
+
+
+EOF
+## ------------------------------------- ##
 
 
 {
-## CREATE A FILE TO EXPORT TO
-export GITHUB_API_RESPONSE_FILE="/tmp/github_api_response_file_${USER_TO_ADD}_$(date -u +'%Y%m%dT%H%M%SZ').txt"
+## THIS IS THE FRAMEWORK FOR THE API CALL TO *accept* THE INVITATION
+## (this is missing just the invitation_id, which is added by the curl responses below)
+## NOTE that in CMD_INVITATION_ACCEPT, the GITHUB_PAT should be changed by the user
+export CMD_INVITATION_ACCEPT="curl -X PUT -H \"Authorization: token \${GITHUB_PAT}\" https://api.github.com/user/repository_invitations/"
 
 echo -e "\033[1;35m"Beginning curl calls to add user to github repos"\033[m"
 
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/collectArgs/collaborators/${USER_TO_ADD}     | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rcreds/collaborators/${USER_TO_ADD}          | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/R_init/collaborators/${USER_TO_ADD}          | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuaspath/collaborators/${USER_TO_ADD}       | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsubitly/collaborators/${USER_TO_ADD}        | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuconsoleutils/collaborators/${USER_TO_ADD} | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsucurl/collaborators/${USER_TO_ADD}         | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsudb/collaborators/${USER_TO_ADD}           | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuaws/collaborators/${USER_TO_ADD}          | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsujesus/collaborators/${USER_TO_ADD}        | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsunotify/collaborators/${USER_TO_ADD}       | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuplotting/collaborators/${USER_TO_ADD}     | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuprophesize/collaborators/${USER_TO_ADD}   | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuscrubbers/collaborators/${USER_TO_ADD}    | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsushiny/collaborators/${USER_TO_ADD}        | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuvydia/collaborators/${USER_TO_ADD}        | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuworkspace/collaborators/${USER_TO_ADD}    | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuxls/collaborators/${USER_TO_ADD}          | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
-curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsugeneral/collaborators/${USER_TO_ADD}      | jq '.id' >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/collectArgs/collaborators/${USER_TO_ADD}     | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rcreds/collaborators/${USER_TO_ADD}          | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/R_init/collaborators/${USER_TO_ADD}          | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuaspath/collaborators/${USER_TO_ADD}       | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsubitly/collaborators/${USER_TO_ADD}        | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuconsoleutils/collaborators/${USER_TO_ADD} | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsucurl/collaborators/${USER_TO_ADD}         | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsudb/collaborators/${USER_TO_ADD}           | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuaws/collaborators/${USER_TO_ADD}          | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsujesus/collaborators/${USER_TO_ADD}        | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsunotify/collaborators/${USER_TO_ADD}       | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuplotting/collaborators/${USER_TO_ADD}     | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuprophesize/collaborators/${USER_TO_ADD}   | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuscrubbers/collaborators/${USER_TO_ADD}    | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsushiny/collaborators/${USER_TO_ADD}        | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuvydia/collaborators/${USER_TO_ADD}        | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuworkspace/collaborators/${USER_TO_ADD}    | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsuxls/collaborators/${USER_TO_ADD}          | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
+curl -X PUT -H "Authorization: token ${GITHUB_PAT}" https://api.github.com/repos/${REPO_OWNER}/rsugeneral/collaborators/${USER_TO_ADD}      | jq '.id' | sed -e "s|^|$CMD_INVITATION_ACCEPT|g" >> "$GITHUB_API_RESPONSE_FILE"
 echo -e "\033[1;97m"================================================================="\033[m"
 
-echo -e "\033[1;35m"The Following invitation IDs have been sent"\033[m"
+echo -e "\033[1;35m"To Accept the invitations, the receiver should run the following"\033[m"
 cat "$GITHUB_API_RESPONSE_FILE"
 echo ""
 echo -e "\033[1;35m"'The tmp file is located here:'"\033[m"  \"${GITHUB_API_RESPONSE_FILE}\"
